5413479f1b919744d8bd335ed0fb2fe8e0ebc918
Prashant Kawade <prashant396@gmail.com>
2020-10-07 17:45:25 +0530

    Added project for tests

---
+++ test/PromotionEngine.Services.Tests/PromotionEngineTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngine.Services.Tests
+{
+	[TestClass]
+	public class PromotionEngineTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
