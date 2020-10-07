1d11810a0a22c8208814ea343f5f3d486ddc39a4
Prashant Kawade <prashant396@gmail.com>
2020-10-07 18:49:20 +0530

    Added models in respective services

--- src/PromotionEngine.Services/Calculation/Implementation/ComboPromotionService.cs
+++ src/PromotionEngine.Services/Calculation/Implementation/ComboPromotionService.cs
@@ -5,6 +5,13 @@ namespace PromotionEngine.Services.Calculation.Implementation
 {
 	public class ComboPromotionService : IPromotion
 	{
+		public ComboPromotion ComboPromotion { get; private set; }
+
+		public ComboPromotionService(ComboPromotion comboPromotion)
+		{
+			ComboPromotion = comboPromotion;
+		}
+
 		public Cart ApplyPromotion(Cart cart)
 		{
 			throw new System.NotImplementedException();
--- src/PromotionEngine.Services/Calculation/Implementation/MultiPromotionService.cs
+++ src/PromotionEngine.Services/Calculation/Implementation/MultiPromotionService.cs
@@ -5,6 +5,14 @@ namespace PromotionEngine.Services.Calculation.Implementation
 {
 	public class MultiPromotionService : IPromotion
 	{
+		public MultiPromotion MultiPromotion { get; private set; }
+
+
+		public MultiPromotionService(MultiPromotion multiPromotion)
+		{
+			MultiPromotion = multiPromotion;
+		}
+
 		public Cart ApplyPromotion(Cart cart)
 		{
 			throw new System.NotImplementedException();
