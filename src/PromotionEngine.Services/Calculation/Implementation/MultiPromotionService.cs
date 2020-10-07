7df95b2d1c35e23d827329d1f60e86796880261d
Prashant Kawade <prashant396@gmail.com>
2020-10-07 18:37:36 +0530

    Renamed to multi promotion service

--- src/PromotionEngine.Services/Calculation/Implementation/MultiPromotionService.cs
+++ src/PromotionEngine.Services/Calculation/Implementation/MultiPromotionService.cs
@@ -3,7 +3,7 @@ using PromotionEngine.Services.Models;
 
 namespace PromotionEngine.Services.Calculation.Implementation
 {
-	public class MultipleItemsPromotionService : IPromotion
+	public class MultiPromotionService : IPromotion
 	{
 		public Cart ApplyPromotion(Cart cart)
 		{
