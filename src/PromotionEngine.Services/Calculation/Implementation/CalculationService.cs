0145cf2979a60fdc990ec6975a6bf0936878fda8
Prashant Kawade <prashant396@gmail.com>
2020-10-07 18:19:32 +0530

    Added implementation with not implemented exception

---
+++ src/PromotionEngine.Services/Calculation/Implementation/CalculationService.cs
@@ -0,0 +1,14 @@
+﻿using PromotionEngine.Services.Calculation.Interfaces;
+using PromotionEngine.Services.Models;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Services.Calculation.Implementation
+{
+	public class CalculationService : ICalculationService
+	{
+		public decimal GetTotal(IEnumerable<Item> items, IEnumerable<IPromotion> promotions)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ src/PromotionEngine.Services/Calculation/Implementation/MultipleItemsPromotionService.cs
@@ -0,0 +1,13 @@
+﻿using PromotionEngine.Services.Calculation.Interfaces;
+using PromotionEngine.Services.Models;
+
+namespace PromotionEngine.Services.Calculation.Implementation
+{
+	public class MultipleItemsPromotionService : IPromotion
+	{
+		public Cart ApplyPromotion(Cart cart)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
