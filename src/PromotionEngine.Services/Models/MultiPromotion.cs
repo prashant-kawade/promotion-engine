75a5dbd50e1b9adff04abab40bfead22307479bc
Prashant Kawade <prashant396@gmail.com>
2020-10-07 18:36:37 +0530

    Added model to save mutli type promotion

---
+++ src/PromotionEngine.Services/Models/MultiPromotion.cs
@@ -0,0 +1,18 @@
+﻿namespace PromotionEngine.Services.Models
+{
+	public class MultiPromotion
+	{
+		public string Name { get; private set; }
+
+		public int Quantity { get; private set; }
+
+		public decimal FixedPrice { get; private set; }
+
+		public MultiPromotion(string name, int quantity, decimal fixedPrce)
+		{
+			Name = name;
+			Quantity = quantity;
+			FixedPrice = fixedPrce;
+		}
+	}
+}
