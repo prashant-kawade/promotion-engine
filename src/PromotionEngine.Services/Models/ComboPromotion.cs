d7de33581ded9e6d5d783824df8988b30e7053cc
Prashant Kawade <prashant396@gmail.com>
2020-10-07 18:40:18 +0530

    Added model to save combo type promotion

---
+++ src/PromotionEngine.Services/Models/ComboPromotion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine.Services.Models
+{
+	class ComboPromotion
+	{
+		public IEnumerable<string> Names { get; private set; }
+
+		public decimal FixedPrice { get; private set; }
+
+		public ComboPromotion(IEnumerable<string> names, decimal fixedPrice)
+		{
+			Names = names;
+			FixedPrice = fixedPrice;
+		}
+	}
+}
