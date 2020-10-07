7ce9399802f810f99e21b9044fbce9435ba878ca
Prashant Kawade <prashant396@gmail.com>
2020-10-07 18:32:31 +0530

    Added dummy products in test ctor

--- test/PromotionEngine.Services.Tests/PromotionEngineTests.cs
+++ test/PromotionEngine.Services.Tests/PromotionEngineTests.cs
@@ -1,10 +1,33 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Services.Calculation.Implementation;
+using PromotionEngine.Services.Calculation.Interfaces;
+using PromotionEngine.Services.Models;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace PromotionEngine.Services.Tests
 {
 	[TestClass]
 	public class PromotionEngineTests
 	{
+		private readonly IEnumerable<Product> _products;
+		private readonly ICalculationService _calculationService;
+
+		public PromotionEngineTests()
+		{
+			var p1 = new Product("A", 50);
+			var p2 = new Product("B", 30);
+			var p3 = new Product("C", 20);
+			var p4 = new Product("D", 15);
+
+			_products = new List<Product>
+			{
+				p1,p2,p3,p4
+			};
+
+			_calculationService = new CalculationService();
+		}
+
 		[TestMethod]
 		public void TestMethod1()
 		{
